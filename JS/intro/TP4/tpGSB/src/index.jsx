ee51daae72e9281047f4e0e16c756d453494985f
Pierre-Yves VASSARD <pierreyves.vassard.pro@gmail.com>
2024-11-07 12:13:21 +0100

    v1.0

---
+++ JS/intro/TP4/tpGSB/src/index.jsx
@@ -0,0 +1,13 @@
+import { createBrowserRouter, RouterProvider, Link } from 'react-router-dom'
+import React from 'react'
+import './index.css'
+
+function Index() {
+    return (
+        <div>
+            <p className=''>ca marche</p>
+        </div>
+    )
+}
+
+export default Index()
